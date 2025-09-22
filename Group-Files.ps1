8e7c65d78b05c83a807ca90f3fdd7a8a9e6e6696
Daniel Balko <inxonic@users.noreply.github.com>
2025-09-22 17:43:10 +0200

    Enable sorting by name
    also split out and document file name normalization

--- Group-Files.ps1
+++ Group-Files.ps1
@@ -35,32 +35,34 @@ param (
     [Int32]$MinCount = 2,
 
     [PSDefaultValue()]
-    [ValidateSet('Count', 'Mark', 'Size')]
+    [ValidateSet('Count', 'Mark', 'Name', 'Size')]
     [String[]]$SortBy = "Count",
 
     [switch]$Descending
 )
+
+filter Normalize-FileName {
+    $_ `
+    <# Remove filename suffix as well as timestamp and station suffix if present #> `
+    -replace '(_\d{4}_(\d{2}_){2}\d{4}_.*)?\.\w+$', '' `
+    <# Remove season and episode information #> `
+    -replace '(^|[\W_])S\d+E\d+($|[\W_])', '$1$2' `
+    <# Normalize all word separators to a single space #> `
+    -replace '[\s-_]+', ' ' `
+    <# Remove any non-word characters except whitespace #> `
+    -replace '[^\w\s]', ''
+}
 
 $sort_parameters = @{
     Property = $SortBy
     Descending = $Descending
 }
 
-$files = Get-ChildItem $Path | Select-Object -Property @{n='File'; e={$_}}, @{n='Mark'; e={$false}}
-$files += Get-ChildItem $MarkedPath | Select-Object -Property @{n='File'; e={$_}}, @{n='Mark'; e={$true}}
-
-$files |
-    Group-Object -Property @{
-        Expression={
-            $_.File.Name `
-                -replace '(_\d{4}_(\d{2}_){2}\d{4}_.*)?\.\w+$', '' `
-                -replace '(^|[\W_])S\d+E\d+($|[\W_])', '$1$2' `
-                -replace '[\s-_]+', ' ' `
-                -replace '[^\w\s]', ''
-        }
-    } |
+(Get-ChildItem $Path | Select-Object -Property @{n='File'; e={ $_ }}, @{n='Mark'; e={ $false }}) +
+    (Get-ChildItem $MarkedPath | Select-Object -Property @{n='File'; e={ $_ }}, @{n='Mark'; e={ $true }}) |
+    Group-Object -Property @{Expression={ $_.File.Name | Normalize-FileName }} |
     Where-Object -Property Count -ge $MinCount |
     Select-Object -Property Count, Name,
-        @{n='Size'; e={(($_.Group.File | Measure-Object -Property Size -Sum).Sum)/1MB}},
-        @{n='Mark'; e={$true -in $_.Group.Mark }} |
+        @{n='Size'; e={ (($_.Group.File | Measure-Object -Property Size -Sum).Sum) / 1MB }},
+        @{n='Mark'; e={ $true -in $_.Group.Mark }} |
     Sort-Object @sort_parameters
