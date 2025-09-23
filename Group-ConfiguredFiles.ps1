b94815b6f39198c1db47fc0b0472e7f02b3901fb
Daniel Balko <inxonic@users.noreply.github.com>
2025-09-23 15:27:18 +0200

    Only evaluate rating in groups with minimal count
    also use correct character set for call to exiftool

--- Group-ConfiguredFiles.ps1
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/pwsh
-
-$ErrorActionPreference = 'Stop'
-$config = Import-PowerShellDataFile (Join-Path $HOME '.Group-Files.conf.psd1')
-&(Join-Path $PSScriptRoot 'Group-Files.ps1') @config @args
--- Group-Files.ps1
+++ Group-Files.ps1
@@ -19,14 +19,14 @@ Specifies the property by which to sort the grouped files
 .PARAMETER Descending
 Sort the grouped files in descending order instead of ascending order
 
+.PARAMETER GetRating
+Retrieve SharedUserRating for marked files and return the highest value per group
+
 .PARAMETER MinCount
 Only return groups containing at least a specific number of files
 
 .PARAMETER MinRating
 Only return groups containing a marked file with at least a specific rating
-
-.PARAMETER CacheRating
-Cache the file ratings per directory
 
 .EXAMPLE
 Group-Files.ps1 -Path C:\Videos -MarkedPath C:\VideoArchive -SortBy Size
@@ -35,7 +35,7 @@ Group-Files.ps1 -Path C:\Videos -MarkedPath C:\VideoArchive -SortBy Size
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory, Position=0, ValueFromPipeline)]
+    [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
     [string[]]$Path,
 
     [Parameter()]
@@ -47,36 +47,39 @@ param (
     [PSDefaultValue()]
     [Int32]$MinCount = 2,
 
-    [PSDefaultValue()]
+    [Parameter()]
     [Int32]$MinRating,
 
     [PSDefaultValue()]
     [ValidateSet('Count', 'Mark', 'Name', 'Rating', 'Size')]
     [String[]]$SortBy = "Count",
 
-    [switch]$Descending,
-    [switch]$CacheRating
+    [switch]$GetRating,
+    [switch]$Descending
 )
 
 begin {
     $ErrorActionPreference = 'Stop'
 
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    $PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
     filter Normalize-FileName {
         $_ `
-        <# Remove filename suffix as well as timestamp and station suffix if present #> `
-        -replace '(_\d{4}_(\d{2}_){2}\d{4}_.*)?\.\w+$', '' `
-        <# Remove season and episode information #> `
-        -replace '(^|[\W_])S\d+E\d+($|[\W_])', '$1$2' `
-        <# Normalize all word separators to a single space #> `
-        -replace '[\s-_]+', ' ' `
-        <# Remove any non-word characters except whitespace #> `
-        -replace '[^\w\s]', ''
+            <# Remove filename suffix as well as timestamp and station suffix if present #> `
+            -replace '(_\d{4}_(\d{2}_){2}\d{4}_.*)?\.\w+$', '' `
+            <# Remove season and episode information #> `
+            -replace '(^|[\W_])S\d+E\d+($|[\W_])', '$1$2' `
+            <# Normalize all word separators to a single space #> `
+            -replace '[\s-_]+', ' ' `
+            <# Remove any non-word characters except whitespace #> `
+            -replace '[^\w\s]', ''
     }
 
     function Get-Files {
         [CmdletBinding()]
         param (
-            [Parameter(Mandatory, Position=0, ValueFromPipeline)]
+            [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
             [string[]]$Path,
 
             [switch]$Mark,
@@ -84,58 +87,59 @@ begin {
         )
 
         process {
-            $rating = @{}
-
-            if ($GetRating) {
-                $ratings_file = Join-Path $Path '.Group-Files.ratings.json'
-
-                if (-not $CacheRating -or -not (Test-Path -PathType Leaf $ratings_file))
-                {
-                    & $ExifTool -charset filename=utf8 -json -SharedUserRating $Path >$ratings_file 2>/dev/null
-                }
-                Get-Content $ratings_file |
-                    ConvertFrom-Json |
-                    ForEach-Object {
-                        $rating[($_.SourceFile | Split-Path -Leaf)] = $_.SharedUserRating
-                    }
-            }
-
             foreach ($file in (Get-ChildItem -File $Path)) {
                 [PSCustomObject]@{
-                    File = $file
-                    Mark = $Mark
-                    Rating = $rating[$file.Name]
+                    File   = $file
+                    Mark   = $Mark
+                    Rating = $null
                 }
             }
         }
     }
 
     $sort_parameters = @{
-        Property = $SortBy
+        Property   = $SortBy
         Descending = $Descending
     }
 
     $files = Get-Files -Path $MarkedPath -Mark -GetRating
 }
 
-process
-{
+process {
     $files += Get-Files -Path $Path
 }
 
 end {
-    $files | Group-Object -Property @{Expression={ $_.File.Name | Normalize-FileName }} |
+    $groups = $files | Group-Object -Property @{Expression = { $_.File.Name | Normalize-FileName } } |
+        Where-Object -Property Count -GE $MinCount
+
+    if ($GetRating) {
+        $ratings = @{}
+        $groups.Group | Where-Object -Property Mark -EQ $true | ForEach-Object { $_.File.FullName } |
+            & $ExifTool -charset filename=utf8 -json -SharedUserRating -@ - 2>$null |
+            ConvertFrom-Json | ForEach-Object {
+                $ratings[$_.SourceFile] = $_.SharedUserRating
+            }
+
+        $groups.Group | Where-Object -Property Mark -EQ $true | ForEach-Object {
+            $_.Rating = $ratings[$_.File.FullName]
+        }
+    }
+
+    $groups |
         ForEach-Object {
-            if ($_.Count -ge $MinCount) {
-                $rating = [int]($_.Group.Rating | Measure-Object -Maximum).Maximum
-                if (-not $MinRating -or $rating -ge $MinRating) {
-                    [PSCustomObject]@{
-                        Mark = $true -in $_.Group.Mark
-                        Rating = $rating
-                        Name = $_.Name
-                        Count = $_.Count
-                        Size = [int](($_.Group.File | Measure-Object -Property Length -Sum).Sum)
-                    }
+            $rating = if ($GetRating) {
+                [int]($_.Group.Rating | Measure-Object -Maximum).Maximum
+            }
+            else { $null }
+
+            if (-not $MinRating -or $rating -ge $MinRating) {
+                [PSCustomObject]@{
+                    Mark   = $true -in $_.Group.Mark
+                    Rating = $rating
+                    Name   = $_.Name
+                    Count  = $_.Count
+                    Size   = [int](($_.Group.File | Measure-Object -Property Length -Sum).Sum)
                 }
             }
 
